a810998b8f3940b2099dbf82b64ab7bf57073c03
Johan Van de Wauw <johan.vandewauw@gmail.com>
2016-11-28 19:23:05 +0100

    Remove unused elements from TPitData
    Saves some memory

--- saga-gis/src/tools/watem/watem/Pitstuff.h
+++ saga-gis/src/tools/watem/watem/Pitstuff.h
@@ -8,10 +8,9 @@ typedef enum {
 } TRoutingAlgorithm;
 
  typedef struct  {
-	long outr, outc, aantal, r, c, PTEF, oldvlag =0;//outr: outrow, outc:outcolumn
-	double input, output, deposition, hoogte =0;
-	bool river = false;
-	long riverid = 0;
+	long outr, outc, aantal, r, c =0;//outr: outrow, outc:outcolumn
+	double input=0;
+
  }  TPitData;
 
 
