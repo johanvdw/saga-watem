c0b0c5f4dcc85ecab29a0010540069eb656dd1e5
Johan Van de Wauw <johan.vandewauw@gmail.com>
2017-07-17 20:24:33 +0200

    update

--- saga-gis/src/tools/terrain_analysis/ta_hydrology/Flow_Fields.h
+++ saga-gis/src/tools/terrain_analysis/ta_hydrology/Flow_Fields.h
@@ -1,11 +1,18 @@
 #include <saga_api/saga_api.h>
 class CFlow_Fields :
-	public CSG_Tool
+	public CSG_Tool_Grid
 {
 public:
 	CFlow_Fields();
 	~CFlow_Fields();
 
-	virtual bool On_Execute(void);
+   
+	double Get_Flow(int, int, double [8]);
+	bool On_Execute();
+private:
+	CSG_Grid * DEM;
+	CSG_Grid * fields;
+	CSG_Grid * UpArea;
+	bool m_bStopAtEdge;
 };
 
