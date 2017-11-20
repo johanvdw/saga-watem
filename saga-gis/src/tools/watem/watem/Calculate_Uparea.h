dc3429b1f9a788c0b009b6f9878668d25bb2371c
Johan Van de Wauw <johan@gisky.be>
2017-11-20 17:44:06 +0100

    Remove unused riverdat stuff

--- saga-gis/src/tools/watem/watem/Calculate_Uparea.h
+++ saga-gis/src/tools/watem/watem/Calculate_Uparea.h
@@ -1,14 +1,6 @@
 #include "MLB_Interface.h"
 #include <vector>
 
-typedef struct  {
-	double latinput, rivinput, rivoutput, bedloadrivinput, bedloadrivoutput,
-		bankerosion, gullycontribution, floodplainloss, totalsedimentload,
-		latuparea, outuparea, inuparea, length, slope, maf, Qb;
-	int segmentid, fnode, tnode, segmentdown, order, check, pondSTE, pondnr_;
-
-} Trivdata;
-
 typedef struct {
 	int outr, outc, aantal, r, c = 0;//outr: outrow, outc:outcolumn
 	double input = 0;
@@ -34,7 +26,6 @@ private:
 	void DistributeTilDirEvent (int i, int j, double *AREA,  double *massbalance
 		);
 	std::vector<TPitData> PitDat;
-	std::vector<Trivdata> RivDat;
 	int pitnum = 0;
 	int pitwin = 200;
 	double TFCAtoCropLand;
