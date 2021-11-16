3856f3b749ca8c1d6c0d9b66b905c10234f5d2ed
Debian <debian@drone-runner>
2021-11-16 11:12:01 +0000

    Try building for focal and bullseye

--- ci/build-upload.sh
+++ ci/build-upload.sh
@@ -1,2 +1,6 @@
-docker build  . -t registry.fluves.net/saga-build
-
+#docker build  . -f Dockerfile-bionic -t registry.fluves.net/drone/saga-watem/build-bionic
+docker build  . -f Dockerfile-bullseye -t registry.fluves.net/drone/saga-watem/build-bullseye
+docker build  . -f Dockerfile-focal -t registry.fluves.net/drone/saga-watem/build-focal
+#docker push registry.fluves.net/drone/saga-watem/build-bionic
+docker push registry.fluves.net/drone/saga-watem/build-bullseye
+docker push registry.fluves.net/drone/saga-watem/build-focal
