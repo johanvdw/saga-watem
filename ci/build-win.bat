b01a5e9776d5c9ec855fe04efa3581f7af1fa061
Johan Van de Wauw <johan@fluves.com>
2022-06-01 16:02:03 +0000

    Copy files to release folder

--- ci/build-win.bat
+++ ci/build-win.bat
@@ -1,1 +1,5 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" saga_vpo.sln /p:Configuration=Release
+mkdir release
+xcopy C:\drone\src\build\network\line_direction\Release\line_direction.dll release
+xcopy C:\drone\src\build\network\topologize\Release\line_direction.dll release
+xcopy C:\drone\src\build\watem\watem\Release\watem.dll release
