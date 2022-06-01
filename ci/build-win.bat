d716c4897e835771e498f3c68f93ddc5fdc0bf37
Johan Van de Wauw <johan@fluves.com>
2022-06-01 14:33:32 +0000

    x9

--- ci/build-win.bat
+++ ci/build-win.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\MSBuild\Current\Bin\msbuild" saga_vpo.sln
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" saga_vpo.sln
