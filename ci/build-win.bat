0f159724564aa0d0437af63b9405e01cb58f9a07
Johan Van de Wauw <johan@gisky.be>
2022-06-02 08:56:05 +0200

    Try creating release zip

--- ci/build-win.bat
+++ ci/build-win.bat
@@ -1,5 +1,6 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" saga_vpo.sln /p:Configuration=Release
 mkdir release
 xcopy C:\drone\src\build\network\line_direction\Release\line_direction.dll release
-xcopy C:\drone\src\build\network\topologize\Release\line_direction.dll release
+xcopy C:\drone\src\build\network\topologize\Release\topologize.dll release
 xcopy C:\drone\src\build\watem\watem\Release\watem.dll release
+powershell Compress-Archive -Path release\*.dll -DestinationPath c:\saga-vpo_%DRONE_TAG%.zip
