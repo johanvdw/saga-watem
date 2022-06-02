b26b065934c11449c922a16b29cedec8145c01ee
Johan Van de Wauw <johan@fluves.com>
2022-06-02 12:17:07 +0000

    Switch to visual studio 2019

--- ci/build-win.bat
+++ ci/build-win.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" saga_vpo.sln /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" saga_vpo.sln /p:Configuration=Release
 mkdir release
 xcopy C:\drone\src\build\network\line_direction\Release\line_direction.dll release
 xcopy C:\drone\src\build\network\topologize\Release\topologize.dll release
