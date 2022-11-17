6e29504ddb17db20a0d56380500f50a7c1e46e29
Johan Van de Wauw <johan@gisky.be>
2022-11-17 11:35:56 +0100

    Improve topology test

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,13 +1,18 @@
 import subprocess
 import os
+import geopandas as gpd
 
 # make sure saga vpo libs are added to path
 os.environ["SAGA_TLB"]="build/network/topologize"
 
 
 def test_topology(tmpdir):
-    cmd = ["saga_cmd","topology","0", "-INPUTLINES", "tests/data/inputlines.shp", "-OUTPUTLINES", tmpdir/"outputlines.shp", "-SYSTEM", "tests/data/mask.rst"]
+    outfile = tmpdir /"outputlines.shp"
+    cmd = ["saga_cmd","topology","0", "-INPUTLINES", "tests/data/inputlines.shp", "-OUTPUTLINES", outfile, "-SYSTEM", "tests/data/mask.rst"]
     result = subprocess.run(cmd,  capture_output=True)
     assert result.returncode==0
-    print(result)
+
+    df=gpd.read_file(str(outfile))
+    assert len(df) == 18
+    assert set(df.columns) == {"start_id", "end_id", "length", "geometry"}
 
