9826adef4d9d965f003fa7691dd04cad4bf8bfa2
Johan Van de Wauw <johan@gisky.be>
2023-02-08 18:00:08 +0100

    Add more tests

---
+++ tests/test_watem.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+from pathlib import Path
+
+import rasterio as rio
+from test_topology import sagify
+
+os.environ["SAGA_TLB"] = "build/watem/watem"
+
+data_dir = Path("tests/data/langegracht")
+
+
+def run_saga(library, module, args):
+    cmd = ["saga_cmd", library, module] + sagify(args)
+    result = subprocess.run(cmd, capture_output=True)
+    assert result.returncode == 0
+
+
+def test_watem_prc(tmpdir):
+    args = {
+        "PARCEL_SHAPES": data_dir / "Adpe768698.shp",
+        "LANDUSE": data_dir / "Landgebruik_def.sgrd",
+        "PRC": tmpdir / "prc.sgrd",
+        "WTZ": data_dir / "Wtze768698.shp",
+        "WLAS": data_dir / "Wlase768698.shp",
+        "SBN": data_dir / "Sbne768698.shp",
+        "WBN": data_dir / "Wbne768698.shp",
+        "WGA": data_dir / "Wgae768698.shp",
+        "GBG": data_dir / "Gbge768698.shp",
+        "GBA": data_dir / "Gbae768698.shp",
+        "TRN": data_dir / "Trne768698.shp",
+        "KNW": data_dir / "Knwe768698.shp",
+    }
+    run_saga("watem", "1", args)
+
+    with rio.open(str(tmpdir / "prc.sdat")) as prc_file:
+        prc = prc_file.read()
+
+    assert prc.shape == (1, 735, 1134)
+
+
+def test_watem_3x3(tmpdir):
+    args = {
+        "DEM": data_dir / "dhmvi_5m.sgrd",
+        "PRC": data_dir / "prc.sgrd",
+        "DEM_FILTER": tmpdir / "dem_filter.sgrd",
+    }
+
+    run_saga("watem", "2", args)
+
+    with rio.open(str(tmpdir / "dem_filter.sdat")) as filtered_file:
+        filtered = filtered_file.read()
+
+    assert filtered.shape == (1, 735, 1134)
+
+
+def test_watem_uparea(tmpdir):
+    args = {
+        "DEM": data_dir / "dhmvi_5m.sgrd",
+        "PRC": data_dir / "prc.sgrd",
+        "PIT": tmpdir / "pit.sgrd",
+        "UPSLOPE_AREA": tmpdir / "upslope_area.sgrd",
+        "PITDATA": tmpdir / "pitdata.txt",
+    }
+    run_saga("watem", "4", args)
+
+
+def test_watem_ls(tmpdir):
+    args = {"DEM": data_dir / "dhmvi_5m.sgrd"}
+    run_saga("watem", "5", args)
