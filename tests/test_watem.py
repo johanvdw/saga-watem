7c9a4a85d984d8aa9cabb6ba00bf87e03dc0ac19
Johan Van de Wauw <johan@gisky.be>
2023-02-16 15:03:00 +0100

    Make sure all tests work at the same time
    clashing environment settings before

--- tests/test_watem.py
+++ tests/test_watem.py
@@ -2,16 +2,17 @@ import os
 import subprocess
 from pathlib import Path
 
+import pytest
 import rasterio as rio
 from test_topology import sagify
 
-os.environ["SAGA_TLB"] = "build/watem/watem"
 
 data_dir = Path("tests/data/langegracht")
 
 
 def run_saga(library, module, args):
     cmd = ["saga_cmd", library, module] + sagify(args)
+    os.environ["SAGA_TLB"] = "build/watem/watem"
     result = subprocess.run(cmd, capture_output=True)
     assert result.returncode == 0
 
@@ -66,5 +67,39 @@ def test_watem_uparea(tmpdir):
 
 
 def test_watem_ls(tmpdir):
-    args = {"DEM": data_dir / "dhmvi_5m.sgrd"}
+    args = {
+        "DEM": data_dir / "dhmvi_5m.sgrd",
+        "PRC": data_dir / "prc.sgrd",
+        "PIT": tmpdir / "pit.sgrd",
+        "UPSLOPE_AREA": tmpdir / "upslope_area.sgrd",
+        "PITDATA": tmpdir / "pitdata.txt",
+    }
+    # test running without slope output
+    run_saga("watem", "4", args)
+
+    args = {"DEM": data_dir / "dhmvi_5m.sgrd",
+            "UPSLOPE_AREA": tmpdir / "upslope_area.sgrd",
+            "USEPRC": "1",
+            "PRC": data_dir / "prc.sgrd",
+            "LS": tmpdir / "ls.sgrd",
+            }
+
+    # test running with slope output
     run_saga("watem", "5", args)
+
+    # test with optional slope
+    args = args | {"SLOPE": tmpdir / "slope.sgrd"}
+    run_saga("watem", "5", args)
+
+    with rio.open(str(tmpdir / "slope.sdat")) as slope_file:
+        slope = slope_file.read()
+    with rio.open(str(tmpdir / "ls.sdat")) as ls_file:
+        ls = ls_file.read()
+    assert slope.shape == (1, 735, 1134)
+    assert ls.shape == (1, 735, 1134)
+
+    # check that the grid contains anything
+    assert pytest.approx(slope[0,100, 200]) == 1e-6
+    assert pytest.approx(ls[0,100, 200]) == 0.030010605
+
+
