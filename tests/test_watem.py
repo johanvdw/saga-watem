79529d3f947e53a2203d666877bfbac6cfbd6a4a
Johan Van de Wauw <johan@gisky.be>
2023-02-17 15:02:49 +0100

    Add some basic tests for parcel c module (watem)

--- tests/test_watem.py
+++ tests/test_watem.py
@@ -12,6 +12,7 @@ data_dir = Path("tests/data/langegracht")
 
 def run_saga(library, module, args):
     cmd = ["saga_cmd", library, module] + sagify(args)
+    print(cmd)
     os.environ["SAGA_TLB"] = "build/watem/watem"
     result = subprocess.run(cmd, capture_output=True)
     assert result.returncode == 0
@@ -103,3 +104,38 @@ def test_watem_ls(tmpdir):
     assert pytest.approx(ls[0,100, 200]) == 0.030010605
 
 
+def test_watem_c_parcel(tmpdir):
+    args = {
+        "PARCEL_SHAPES": data_dir / "Adpe768698.shp",
+        "LANDUSE": data_dir / "Landgebruik_def.sgrd",
+        "C": tmpdir / "c.sgrd",
+        "WTZ": data_dir / "Wtze768698.shp",
+        "WLAS": data_dir / "Wlase768698.shp",
+        "SBN": data_dir / "Sbne768698.shp",
+        "WBN": data_dir / "Wbne768698.shp",
+        "WGA": data_dir / "Wgae768698.shp",
+        "GBG": data_dir / "Gbge768698.shp",
+        "GBA": data_dir / "Gbae768698.shp",
+        "TRN": data_dir / "Trne768698.shp",
+        "KNW": data_dir / "Knwe768698.shp",
+        "GRB_VHA": "1"
+    }
+    run_saga("watem", "9", args)
+
+    with rio.open(str(tmpdir / "c.sdat")) as c_file:
+        c = c_file.read()
+
+    assert c.shape == (1, 735, 1134)
+
+    args = {
+        "PARCEL_SHAPES": data_dir / "Adpe768698.shp",
+        "LANDUSE": data_dir / "Landgebruik_def.sgrd",
+        "C": tmpdir / "c.sgrd",
+        "GRB_VHA": "0"
+    }
+    run_saga("watem", "9", args)
+
+    with rio.open(str(tmpdir / "c.sdat")) as c_file:
+        c = c_file.read()
+
+    assert c.shape == (1, 735, 1134)
