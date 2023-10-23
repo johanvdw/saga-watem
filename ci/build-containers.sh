9b83910ffbc6f131c483ddf7606dae727ddeaf7f
Johan Van de Wauw <johan@fluves.com>
2023-10-23 18:43:36 +0000

    Add jammy as target

--- ci/build-containers.sh
+++ ci/build-containers.sh
@@ -1,4 +1,4 @@
-for i in "debian:bookworm" "debian:bullseye" "ubuntu:focal";
+for i in "debian:bookworm" "debian:bullseye" "ubuntu:focal" "ubuntu:jammy";
 do 
 	podman build --build-arg base_container=$i -t ghcr.io/johanvdw/saga-watem-build-$i .
 	podman push ghcr.io/johanvdw/saga-watem-build-$i
