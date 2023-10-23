f1061157aa1ccaabba42f701ae81b86369f824d8
Johan Van de Wauw <johan@fluves.com>
2023-10-23 19:07:22 +0200

    Update docker to include distutils

--- ci/build-upload.sh
+++
@@ -1,6 +0,0 @@
-#docker build  . -f Dockerfile-bionic -t registry.fluves.net/drone/saga-watem/build-bionic
-docker build  . -f Dockerfile-bullseye -t registry.fluves.net/drone/saga-watem/build-bullseye
-docker build  . -f Dockerfile-focal -t registry.fluves.net/drone/saga-watem/build-focal
-#docker push registry.fluves.net/drone/saga-watem/build-bionic
-docker push registry.fluves.net/drone/saga-watem/build-bullseye
-docker push registry.fluves.net/drone/saga-watem/build-focal
